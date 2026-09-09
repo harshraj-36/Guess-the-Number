bb040b31699c0c1d521649d724478b5b7e70849c
Harsh Raj <harshraj15112@gmail.com>
2026-09-10 04:36:32 +0530

    Script added and html file created

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
+const getRandomNumber= function(min, max)
+{
+    min=Math.ceil(min);
+    max=Math.floor(max);
 
+    return Math.floor(Math.random() * (max-min+1) + min) 
+    //In the above code I have written the code for genearating a random number.
+}
+
+let RandomNumber= getRandomNumber(1, 100)
+let wrongans=0
+let score=0
+let guessnum
+
+while(RandomNumber!=guessnum)   
+    /* The logic is that the loop must run until the guessed number is not equals to randomly generated number. And once it is equals to it will break the loop and come out of loop. */
+
+{
+    guessnum=prompt("Enter the number you guess: ")
+    guessnum=Number.parseInt(guessnum)
+
+    if(RandomNumber>guessnum)
+    {
+        alert("The number you guessed is lesser than the original number.")
+        wrongans++;
+    }
+
+    else if(RandomNumber<guessnum)
+    {
+        alert("The number you guessed is greater than the original number.")
+        wrongans++;
+    }
+
+    else if(RandomNumber==guessnum)
+    {
+        alert("Congratulations! You hav guessed the number.")
+        score=100-wrongans;
+        alert("Your score is: ", score)
+    }
+
+    else
+    {
+        alert("You have entered an invalid character.")
+        wrongans++;
+    }
+}
